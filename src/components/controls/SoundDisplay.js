616561286d3d0507686323ff6c6d8574aed0cedc
Xander T <xandert.93@outlook.com>
2021-04-12 19:54:00 +0100

    add media queries

--- src/components/controls/SoundDisplay.js
+++ src/components/controls/SoundDisplay.js
@@ -3,19 +3,27 @@ import React, { Component, createRef } from 'react';
 export class SoundDisplay extends Component {
   constructor(props) {
     super(props);
+    // this.state = {
+    //   screenWidth: window.innerWidth,
+    // };
     this.displayRef = createRef();
   }
 
   render() {
     return (
       <div ref={this.displayRef} className="sound-display">
-        Hit a Key...
+        {/* {this.state.screenWidth < 1200 ? 'Touch' : 'Press/Click'} a key! */}
+        {/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+          ? 'Touch'
+          : 'Press/Click'}{' '}
+        a key!
       </div>
     );
   }
 
   componentDidMount() {
     this.props.setDisplayNode(this.displayRef.current);
+    // window.onresize = () => this.setState({ screenWidth: window.innerWidth });
   }
 }
 
